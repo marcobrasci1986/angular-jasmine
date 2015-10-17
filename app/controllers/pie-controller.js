2f80c4e142d53ac601b16934a03553f32ef01af3
marcobrasci1986 <marco_brasci@hotmail.com>
2015-10-17 18:32:09 +0200

    adds filter + tests adds code coverage config

--- app/controllers/pie-controller.js
+++ app/controllers/pie-controller.js
@@ -1,4 +1,4 @@
-var app = angular.module('pie', []);
+
 
 app.controller('PieController', function ($scope) {
 
--- karma.conf.js
+++ karma.conf.js
@@ -31,13 +31,14 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      '**/*.js' : ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
@@ -54,7 +55,7 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: "no",
+    autoWatch: "true",
 
 
     // start these browsers
