211ff8f25c44abde1037d988ae111dd4d45faadb
marcobrasci1986 <marco_brasci@hotmail.com>
2015-10-25 09:59:27 +0100

    fixing protractor test

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,9 +1,17 @@
+// https://www.credera.com/blog/technology-insights/java/testing-angularjs-part-4-setting-e2e-testing-protractor/
+// Before you can run your tests, your app will need to be running on a web server.
+
 exports.config = {
 
     specs: [
         'e2e/**/*spec.js'
     ],
 
-    baseUrl: 'http://localhost:3333/angular-jasmine/'
+    capabilities: {
+        'browserName': 'chrome'
+    },
+
+    seleniumAddress: 'http://localhost:4444/wd/hub', // default
+    baseUrl: 'http://localhost:63342/angular-jasmine/app/'
 
 }
